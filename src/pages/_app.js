fd6ca46d86749b7f080acc82c79b81c3da59d59b
Michael <mb822@njit.edu>
2023-12-07 12:18:09 -0500

    Created landing page closes #23

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,12 @@
-import type { AppProps } from "next/app";
-
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import {useRouter} from 'next/router';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider>
-        <Component {...pageProps} />
-      </NextThemesProvider>
+      <Component {...pageProps} />
     </NextUIProvider>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+export default function LandingPage() {
+  return (
+    <>
+      <h1>
+        Welcome to FitFarm!
+      </h1>
+      <h4>
+        Home of the Eggcelent Eggs.
+      </h4>
+    </>
+  );
+}
