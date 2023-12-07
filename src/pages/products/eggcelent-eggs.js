13eb391646e9f55163f8f3ecc21999e79ad2cb6b
Michael <mb822@njit.edu>
2023-12-07 12:26:05 -0500

    Created Eggcelent Eggs product page closes #24

---
+++ src/pages/products/eggcelent-eggs.js
@@ -0,0 +1,12 @@
+export default function EggcelentEggs() {
+  return (
+    <>
+      <h1>
+        Eggcelent Eggs
+      </h1>
+      <h4>
+        The Eggs that give you Gains!
+      </h4>
+    </>
+  );
+}
