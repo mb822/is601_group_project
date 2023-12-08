19d881243e0f799100c2187e26717b9ed614bfcd
Michael <mb822@njit.edu>
2023-12-08 16:22:48 -0500

    Added CookieConsent component closes #30

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
 import { NextUIProvider } from "@nextui-org/react";
 import {useRouter} from 'next/router';
+import CookieConsent from 'react-cookie-consent';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="FitFarmCookieConsent"
+        expires={365}
+      >
+        This website uses cookies to enhance your experience. By clicking "Accept," you agree to the use of cookies on this site. We use cookies to analyze website traffic, personalize content, and serve targeted advertisements. You can learn more about our use of cookies and manage your preferences in our [Privacy Policy].
+      </CookieConsent>
       <Component {...pageProps} />
     </NextUIProvider>
   );
