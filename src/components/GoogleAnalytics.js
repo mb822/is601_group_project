f76b779d38ef80f3c3a47b50656f31b676dd4817
Michael <mb822@njit.edu>
2023-12-08 23:46:58 -0500

    Signaling navigation to GA closes #35

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,16 +1,26 @@
 import Script, { ScriptProps } from "next/script";
+import {usePathname, useSearchParams} from 'next/navigation';
 import {useState, useEffect} from 'react';
+import {pageview} from '@/utils/ga4.js';
 import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
 
 const GoogleAnalytics = () => {
   const [cookieConsent, setCookieConsent] = useState(getCookieConsentValue('FitFarmCookieConsent') || false);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-    console.log( cookieConsent ? 'granted' : 'denied');
+  useEffect(() => {
+    console.log( cookieConsent ? 'cc granted' : 'cc denied');
     window.gtag('consent', 'update', {
       'analytics_storage': cookieConsent ? 'granted' : 'denied'
     });
   },[cookieConsent]);
+
+  useEffect(() => {
+    console.log("navigation occured");
+    const url = pathname + searchParams.toString();
+    pageview('G-PWLN5LZHSX', url);
+  },[pathname, searchParams]);
 
   return (
     <>
