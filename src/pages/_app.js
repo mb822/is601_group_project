2157e244191d1a947ad0dc145b8e1d010d626e2a
Michael <mb822@njit.edu>
2023-12-08 23:10:14 -0500

    Added GA component to load scripts closes #32

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { NextUIProvider } from "@nextui-org/react";
 import {useRouter} from 'next/router';
 import CookieConsent from 'react-cookie-consent';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
       >
         This website uses cookies to enhance your experience. By clicking "Accept," you agree to the use of cookies on this site. We use cookies to analyze website traffic, personalize content, and serve targeted advertisements. You can learn more about our use of cookies and manage your preferences in our [Privacy Policy].
       </CookieConsent>
+      <GoogleAnalytics/>
       <Component {...pageProps} />
     </NextUIProvider>
   );
