242435c94e15389114aade3205d72ad26ce32d60
Michael <mb822@njit.edu>
2023-12-15 16:22:45 -0500

    Added description meta tag to landing page closes #51

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default function LandingPage() {
     <>
       <Head>
         <title>FitFarm - The Ultimate Whole Food Sources</title>
+        <meta name="description" content="Saddle up for a wild ride with FitFarm's Eggceleent eggs, the outlaw of nutritional treasures. These bad boys in blue shells are the renegades of the coop, delivering a powerful punch of essential nutrients straight from free-spirited, naturally-raised hens. Unleash the untamed goodness and make every meal an adventure with these outlaw eggs." />
       </Head>
       <h1>
         Welcome to FitFarm!
